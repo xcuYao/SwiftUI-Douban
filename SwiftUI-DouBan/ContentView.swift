8292d1afb3f42a42b1384669bea25be3a6c1deb2
yaoning <chinayaoning@gmail.com>
2021-12-20 19:57:07 +0800

    Initial Commit

---
+++ SwiftUI-DouBan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-DouBan
+//
+//  Created by yaoning on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-DouBan/SwiftUI_DouBanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DouBanApp.swift
+//  SwiftUI-DouBan
+//
+//  Created by yaoning on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DouBanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
