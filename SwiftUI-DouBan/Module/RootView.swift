6e80b0a590e3bb27b349b8c1de324e43ff1d8ac5
yaoning <chinayaoning@gmail.com>
2022-02-20 23:24:27 +0800

    menuView ing

--- SwiftUI-DouBan/Module/RootView.swift
+++ SwiftUI-DouBan/Module/RootView.swift
@@ -72,11 +72,9 @@ struct MenuView: View {
                     }
                 }
             VStack {
-                Text("MenuView")
-                Text("MenuView")
-                Text("MenuView")
-                Text("MenuView")
+                MenuTopView()
                 Spacer()
+                MenuBottomView()
             }.frame(width: 220)
             .background(Color(hex: "#F7F7F7"))
         }
@@ -85,8 +83,109 @@ struct MenuView: View {
     }
 }
 
+
+struct MenuTopView: View {
+    var body: some View {
+        VStack {
+            HStack {
+            }.frame(height: DouBan.statusBarHeight)
+                .background(Color.red)
+            HStack() {
+                Image("search_icon")
+                Spacer()
+                Image("search_icon")
+            }.padding([.leading, .trailing], 20)
+            HStack() {
+                Image("search_icon")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .background(Color.red)
+                    .cornerRadius(20)
+                VStack(alignment: .leading) {
+                    Text("Neo")
+                        .font(.system(size: 18))
+                    Text("资料与账号")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color.gray)
+                }
+                Spacer()
+            }.padding([.leading], 20)
+            VStack(alignment: .leading, spacing: 10) {
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("我的收藏/豆列")
+                    Spacer()
+                }
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("我的关注")
+                    Spacer()
+                }
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("浏览历史")
+                    Spacer()
+                }
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("青少年模式")
+                    Spacer()
+                }
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("设置")
+                    Spacer()
+                }
+                HStack(spacing: 14) {
+                    Image("search_icon")
+                    Text("帮助与反馈")
+                    Spacer()
+                }
+            }.padding([.leading, .top], 20)
+        }
+    }
+}
+
+struct MenuBottomView: View {
+    var body: some View {
+        VStack {
+            VStack() {
+                HStack {
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                }.padding([.leading, .trailing], 6)
+                    .background(Color.red)
+                HStack {
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                    Text("订单")
+                        .frame(maxWidth: .infinity)
+                }.padding([.leading, .trailing], 6)
+                    .background(Color.red)
+            }.padding([.leading, .trailing], 6)
+                .cornerRadius(8)
+            HStack {
+                Text("豆瓣")
+                Text("证照信息>")
+            }.padding([.top], 20)
+        }
+    }
+}
+
+//struct RootView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RootView()
+//    }
+//}
+
 struct RootView_Previews: PreviewProvider {
     static var previews: some View {
-        RootView()
+        MenuBottomView()
     }
 }
