e6d2e91efb0aea3e2665c0ef0c7cad25debacb71
yaoning <chinayaoning@gmail.com>
2022-03-07 19:41:30 +0800

    add pager

--- SwiftUI-DouBan/Module/Home/HomeBody.swift
+++ SwiftUI-DouBan/Module/Home/HomeBody.swift
@@ -24,30 +24,6 @@ struct HomeBody: View {
                 }
             }
         }
-//        ZStack(alignment: .bottom, content: {
-//            Image("avatar")
-//            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//                .foregroundColor(Color.white)
-//                .padding(10)
-//                .background(Color.red)
-//                .cornerRadius(10)
-//            Button(action: {
-//                showToast.toggle()
-//            }, label: {
-//                    Text("Show Toast")
-//                })
-//        }).simpleToast(isPresented: $showToast, options: toastOptions, onDismiss: {
-//            print("toast dismiss")
-//        }, content: {
-//                HStack {
-//                    Image(systemName: "exclamationmark.triangle")
-//                    Text("This is some simple toast message.")
-//                }
-//                    .padding()
-//                    .background(Color.red.opacity(0.8))
-//                    .foregroundColor(Color.white)
-//                    .cornerRadius(10)
-//            })
     }
 }
 
--- SwiftUI-DouBan/Module/Home/HomeView.swift
+++ SwiftUI-DouBan/Module/Home/HomeView.swift
@@ -10,15 +10,64 @@ import SwiftUI
 struct HomeView: View {
 
     @Binding var menuToggle: Bool
-
+    @State private var selection = 0
+    
     var body: some View {
         VStack {
             HomeHeader(menuToggle: $menuToggle)
                 .frame(height: 40)
                 .clipped()
-            HomeBody()
-            Spacer()
+
+            PagerTabStripView(swipeGestureEnabled: true, selection: $selection, content: {
+                HomeBody().pagerTabItem {
+                    DoubanNavBarItem(title: "动态")
+                }
+                HomeBody2().pagerTabItem {
+                    DoubanNavBarItem(title: "关注")
+                }
+            })
         }.statusBar(hidden: menuToggle)
+    }
+}
+
+struct DoubanNavBarItem: View, PagerTabViewDelegate {
+
+    let title: String
+
+    @State private var textColor = Color.red
+
+    var body: some View {
+        VStack {
+            Text(title)
+                .foregroundColor(textColor)
+                .font(.system(size: 14))
+        }
+            .background(Color.clear)
+    }
+
+    func setState(state: PagerTabViewState) {
+        switch state {
+        case .selected:
+            self.textColor = Color.black
+        case .highlighted:
+            self.textColor = Color.yellow
+        default:
+            self.textColor = Color.red
+        }
+    }
+
+}
+
+struct HomeBody2: View {
+    var body: some View {
+        ZStack(alignment: .bottom, content: {
+            Image("avatar")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.white)
+                .padding(10)
+                .background(Color.red)
+                .cornerRadius(10)
+        })
     }
 }
 
